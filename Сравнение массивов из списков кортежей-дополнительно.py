7de11bc347c562e96e906947d1a4722dfc29f819
Zekkard <48284505+Zekkard@users.noreply.github.com>
2019-05-29 16:00:06 +0300

    Add files via upload

---
+++ Сравнение массивов из списков кортежей-дополнительно.py
@@ -0,0 +1,41 @@
+# ¬тора€ верси€ задачи про массив из списка кортежей со временными интервалами. 
+# ¬ задаче требовалось сравнить два массива и получить пересечение временных отрезков.
+# ”точнили форму вывода, осуществл€тьс€ должен списком из кортежей.
+
+
+# ћодули.
+import numpy as np
+from itertools import groupby
+from operator import itemgetter, sub
+
+# ћассив, в котором хранитс€ список из кортежей. ѕр€ма€ копи€ из задани€.
+var1 = np.array([(6,8), (10,15), (19,23)]) 
+var2 = np.array([(7,12), (19,22)])
+Lst1 = []
+Lst2 = []
+Answer_pool = []
+Answer = []
+
+# —читывание массивов и вычисление последовательностей дл€ каждого отдельного кортежа с преобразованием результата в множества.
+for (w, x) in var1:
+    Lst1.append(np.arange(w,x+1))
+tup1 = np.concatenate(Lst1, axis=0)
+tup_con1 = set(tuple(tup1))
+for (y, z) in var2:
+    Lst2.append(np.arange(y,z+1))
+tup2 = np.concatenate(Lst2, axis=0)
+tup_con2 = set(tuple(tup2))
+
+# —равнение множеств.
+Digits_ranges = (tup_con1 & tup_con2)
+
+# ќпределение минимум-максимум последовательности из списка, создание из списка кортежей.
+for q in Digits_ranges:
+    Answer_pool.append(int(q))
+for d, e in groupby(enumerate(Answer_pool), lambda x: sub(*x)):
+    items = list(map(itemgetter(1), e))
+    if len(items) > 1:
+        Answer.append( (items[0],items[-1]) )
+    else:
+        Answer.append((items[0]))
+print(Answer)
